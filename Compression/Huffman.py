f3c459ac3c7165904e74dafd5c4e815a1ec889c5
juanmedina1810 <jumedina@itba.edu.ar>
2021-10-15 19:29:27 -0300

    new changes

--- Compression/Huffman.py
+++ Compression/Huffman.py
@@ -1,9 +1,6 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from scipy import signal
-import SimpleITK as sitk
-import math
 
 """
 Huffman Algorithm for image compression
@@ -13,40 +10,46 @@ Huffman Algorithm for image compression
 
 image = cv2.imread('/Users/juanmedina1810/PycharmProjects/PIB/Compression/Img2.png', 0)
 
-print(image)
-
-vals = image.mean(axis=1).flatten()
-# plot histogram with 255 bins
-b, bins, patches = plt.hist(image, 255)
-plt.xlim([0,255])
-plt.show()
 
 # comprimida,dicc,shape = Huffman(img) # Asi tenemos que aplicar el algoritmo
 # descomprimida = HuffmanDec(comprimida,dicc,shape) # Asi lo vamos a descomprimir (Es una funcion nueva)
 
-"""
+def findCode(tree, val):
+    code, padre = tree[val]
+    if padre != 'r':
+        code = findCode(tree, padre) + code
+    return code
+
+
 def Huffman(img):
     # Paso 1, tamaño de la imagen
-    
-    shape_x = img.get_shape[0]
-    shape_y = img.get_shape[1]
+    shape = img.shape
 
     # paso 2, Calculo el histograma normalizado
-    
-    plt.hist(np.ravel(image),bins=256, range=(0,255)), plt.title('Histograma de la imagen')
-    plt.show()
-    
+
+    _, vector, _a = plt.hist(np.ravel(image), bins=256, range=(0, 255))
+
     # paso 3, Genero dos listas vacias para las frecuencias y para la intensidad correspondiente
 
-    # paso 4, ingreso valores en las listas
+    Lista_intensidad = []  # Se guardan los valores de intensidad de gris
+    Lista_histograma = []  # Se guarda la frecuencia con el nivel de gris
+
+    # paso 4, ingreso valores en las listas => Estaria bueno llenar solo los que tienen valores != 0
+
+    for i in range(len(vector)):
+        Lista_intensidad.append(vector[i])
+        Lista_intensidad.append(i)
 
     # paso 5,Creo mi arbol y el contador para los nodos
+
     tree = {}
     k = 0
 
     # paso 6, While que genera el arbol
-    while (len(Lista_histograma) > 1):
+    while len(Lista_histograma) > 1:
+
         # Ordeno Histograma segun la frecuencia (hist(i)) ordenar de menor a mayor
+        Lista_histograma = sorted(Lista_histograma)
 
         # Selecciono los primeros dos, saco la frecuencia conjunta y los vuelvo a agregar a la lista para que se reordenen en el while
         a1 = Lista_histograma.pop()
@@ -57,6 +60,7 @@ def Huffman(img):
         nstr = 'n' + kstr
         n = [fconj, nstr]
         Lista_histograma.append(n)
+
         # Armo el arbol
         tree[a1[1]] = ['0', nstr]
         tree[a2[1]] = ['1', nstr]
@@ -72,8 +76,15 @@ def Huffman(img):
         code = findCode(tree, a)
         dicc[a] = code
 
+    print(dicc)
+
     # paso 10, Recorro la imagen y genero una lista de string con los valores que le corresponden
 
+    # Si tenemos esto ya esta el algorithm
+
     # paso 11
-    return comprimida, dicc, shape
-"""
+
+    # return comprimida, dicc, shape
+
+
+Huffman(image)
