b3da8cb8ca65ea24b6fce514605f8778c1a3023f
juanmedina1810 <jumedina@itba.edu.ar>
2021-10-16 19:25:36 -0300

    RLEH and RLEV ready, RLEZZ not working properly

--- Compression/RLE.py
+++ Compression/RLE.py
@@ -33,8 +33,7 @@ def ComprimirLRE_HV(img, modo):
                     value = gris
                     cant = 1
 
-
-    # Horizontal
+    # Vertical
     if modo == 1:
         modo = "Vertical"
         fil, col = img.shape
@@ -59,6 +58,72 @@ def ComprimirLRE_HV(img, modo):
 
     return listaRLE
 
+def comprimirRLE_ZZ(img):
 
-comp = ComprimirLRE_HV(image, 1)
-print(comp)
+    modo = "Zig-Zag"
+    fil, col = img.shape
+    dtype = img.dtype
+    listaRLE = [[fil, col, modo, dtype], []]
+
+    return listaRLE
+
+def zig_zag_list(matrix):
+    fil, col = matrix.shape
+    solution = [[] for i in range(fil + col - 1)]
+
+    for i in range(fil):
+        for j in range(col):
+            sum = i + j
+            if (sum % 2 == 0):
+
+                # add at beginning
+                solution[sum].insert(0, matrix[i][j])
+            else:
+                # add at end of the list
+                solution[sum].append(matrix[i][j])
+
+    return solution
+
+#comp = comprimirRLE_ZZ(image)
+
+matrix = np.array([
+    [1, 2, 5],
+    [2, 4, 1],
+    [4, 1, 9],
+])
+
+print("Tenemos la siguiente matrix\n")
+print(matrix)
+
+solution = zig_zag_list(matrix)
+
+print("La solucion:",solution)
+# print the solution as it as
+
+# Desde aca opero sobre la solucion
+modo = "Zig-Zag"
+dtype = type(matrix)
+lista_salida = [[fil, col, modo, dtype],[]]
+
+cant = 0
+for i in range(len(solution)):
+    value = solution[i][0]
+    componentes = ""
+    print("Tenemos xx",solution[i])
+    if len(solution[i]) == 1:
+        lista_salida[1].append([solution[i][0],1])
+    else:
+        for j in range(len(solution[i])):
+            print("Tenemos",solution[i])
+            print("j:",j)
+            gris = solution[i][j]
+            if gris == value:
+                cant += 1
+            else:
+                lista_salida[1].append([value, cant])
+                value = gris
+                cant = 1
+
+
+print("La salida es:",lista_salida)
+
