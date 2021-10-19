7c58f1b9ade18df299fd972a376835faea391187
juanmedina1810 <jumedina@itba.edu.ar>
2021-10-18 23:26:44 -0300

    solo falta la decodificacion

--- Compression/RLE.py
+++ Compression/RLE.py
@@ -12,7 +12,7 @@ def ComprimirLRE_HV(img, modo):
         modo = "Horizontal"
         fil, col = img.shape
         dtype = img.dtype
-        listaRLE = [[fil, col, modo, dtype],[]]
+        listaRLE = [[fil, col, modo, dtype], []]
 
         value = img[0, 0]
         cant = 0
@@ -24,7 +24,7 @@ def ComprimirLRE_HV(img, modo):
                     cant += 1
                 else:
 
-                    listaRLE[1].append([value,cant])
+                    listaRLE[1].append([value, cant])
 
                     value = gris
                     cant = 1
@@ -54,7 +54,8 @@ def ComprimirLRE_HV(img, modo):
 
     return listaRLE
 
-def comprimirRLE_ZZ(img):
+
+def comprimirRLE_ZZ(img):  # No lo estoy usando
 
     modo = "Zig-Zag"
     fil, col = img.shape
@@ -62,6 +63,7 @@ def comprimirRLE_ZZ(img):
     listaRLE = [[fil, col, modo, dtype], []]
 
     return listaRLE
+
 
 def zig_zag_list(matrix):
     fil, col = matrix.shape
@@ -80,6 +82,7 @@ def zig_zag_list(matrix):
 
     return solution
 
+
 def encoder_rle(lista):
     i = 0
     contador = 1
@@ -87,7 +90,7 @@ def encoder_rle(lista):
     while i < len(lista):
         value = lista[i]
 
-        if i == len(lista) - 1 and contador ==1:
+        if i == len(lista) - 1 and contador == 1:
             lista_out.append([lista[i], contador])
             break
 
@@ -99,12 +102,13 @@ def encoder_rle(lista):
             contador += 1
 
         if value != lista[i + 1] or i == len(lista):
-            lista_out.append([lista[i],contador])
+            lista_out.append([lista[i], contador])
             contador = 1
 
         i += 1
 
     return lista_out
+
 
 def decoder_rle(lista):
     list_decoded = []
@@ -117,15 +121,14 @@ def decoder_rle(lista):
             k += 1
     return list_decoded
 
-def RLE_encoder(matrix):
 
-    solution = zig_zag_list(matrix) # Esta es la lista de listas, de las diagonales
+def RLE_encoder(matrix):
+    solution = zig_zag_list(matrix)  # Esta es la lista de listas, de las diagonales
     # Desde aca opero sobre la solucion
     fil, col = matrix.shape
     modo = "Zig-Zag"
     dtype = type(matrix)
     lista_salida = [[fil, col, modo, dtype], []]
-    print("Teniendo una lista solution",solution)
 
     for i in range(len(solution)):
         lista_salida[1].append(encoder_rle(solution[i]))
@@ -135,9 +138,45 @@ def RLE_encoder(matrix):
 
 # Una matrix simil imagen
 matrix = np.array([
-    [1, 2, 7],
-    [4, 7, 6],
+    [1, 2, 3],
+    [4, 5, 6],
     [7, 8, 9],
 ])
+print(matrix)
+print("Tenemos:", RLE_encoder(matrix))
 
-print("Tenemos:",RLE_encoder(matrix))
+comprimida = RLE_encoder(matrix)
+# Para decodificar yo tengo que hacer un decoder_rle(encoder_rle(solution[i]))
+
+# Asi queda codificada ==>lista_salida = [[fil, col, modo, dtype], [D1,D2,...,Dn]]
+
+def RLE_decoder(lista_salida):
+    lista_decoded = []
+    for i in range(len(lista_salida[1])):
+        lista_decoded.append(decoder_rle(lista_salida[1][i]))
+
+    # Tenemos la lista decoded que es igual a la lista solucion de antes
+
+    fil = lista_salida[0][0]
+    col = lista_salida[0][1]
+    matrix = np.zeros(shape=(fil, col))
+    print(matrix)
+
+    for i in range(fil):
+        for j in range(col): # Voy recorriendo la matrix
+            for k in range(len(lista_decoded)):  # Recorro la lista decodificada
+                sum = i + j
+                if (sum % 2 == k):
+                    # add at beginning
+                    pass
+                else:
+                    pass
+                    # add at end of the list
+
+
+    return lista_decoded
+
+print("Decoded:",RLE_decoder(comprimida))
+
+
+# Nos queda igual a la lista solucion, asi que tenemos que poder pasar eso a la matrix
