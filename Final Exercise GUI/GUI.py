7d463e70b01c338575f54ab7d9b6813bf7106e5f
juanmedina1810 <jumedina@itba.edu.ar>
2021-11-21 18:40:18 -0300

    final gui - Commit 3

--- Final Exercise GUI/GUI.py
+++ Final Exercise GUI/GUI.py
@@ -1,21 +1,34 @@
 from tkinter import *
+from PIL import ImageTk,Image
 
 root = Tk()
 # Definir un texto
 myLabel = Label(root, text="Hello world")
 myLabel.pack()
 
-texto = Entry(root, width = 50)# Casilla para ingresar texto
+texto = Entry(root, width=50)  # Casilla para ingresar texto
 texto.pack()
+
 
 # Definir un boton
 # Podemos cambiarle el tamaño
 # Para que tenga alguna actividad tenes que definir una funcion!
 def onClick():
-    label = Label(root, text = "Hola! " + texto.get())
+    label = Label(root, text="Hola! " + texto.get())
     label.pack()
+
 
 # El parametro command define la funcion que tiene asignada
 myButton = Button(root, text="Enter your name", padx=50, pady=20, command=onClick)
 myButton.pack()
+
+exit_button = Button(root, text="Exit", command=root.quit)
+exit_button.pack()
+
+# Para cargar imagenes!
+
+
+
+
+
 root.mainloop()
