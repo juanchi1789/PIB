f3d8c88db6ab778ac374f9ff7cde43ea297d4e6b
juanmedina1810 <jumedina@itba.edu.ar>
2021-11-21 23:25:34 -0300

    final gui - Commit 6 - Solo queda cargar las funciones y retocar

--- Final Exercise GUI/GUI.py
+++ Final Exercise GUI/GUI.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import tkinter
+from tkinter import filedialog
 import PIL
 from PIL import ImageTk,Image
 import numpy as np
@@ -52,50 +52,55 @@ canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
 
 root.mainloop()
 """
-
+#global panelA, panelB
 def select_image():
-	# grab a reference to the image panels
-	global panelA, panelB
-	# open a file chooser dialog and allow the user to select an input
-	# image
-	path = tkFileDialog.askopenfilename()
-    # ensure a file path was selected
-    if len(path) > 0:
-    # load the image from disk, convert it to grayscale, and detect
-    # edges in it
-    image = cv2.imread(path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    edged = cv2.Canny(gray, 50, 100)
-    # OpenCV represents images in BGR order; however PIL represents
-    # images in RGB order, so we need to swap the channels
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # convert the images to PIL format...
-    image = Image.fromarray(image)
-    edged = Image.fromarray(edged)
-    # ...and then to ImageTk format
-    image = ImageTk.PhotoImage(image)
-    edged = ImageTk.PhotoImage(edged)
-    # if the panels are None, initialize them
+    # grab a reference to the image panels
+    global panelA, panelB
+    # open a file chooser dialog and allow the user to select an input image
 
-    if panelA is None or panelB is None:
-        # the first panel will store our original image
-        panelA = Label(image=image)
-        panelA.image = image
-        panelA.pack(side="left", padx=10, pady=10)
-        # while the second panel will store the edge map
-        panelB = Label(image=edged)
-        panelB.image = edged
-        panelB.pack(side="right", padx=10, pady=10)
-    # otherwise, update the image panels
-    else:
-        # update the pannels
-        panelA.configure(image=image)
-        panelB.configure(image=edged)
-        panelA.image = image
-        panelB.image = edged
+    path = filedialog.askopenfilename()
+    if len(path) > 0:
+        # load the image from disk, convert it to grayscale, and detect
+        # edges in it
+        image_inicial = cv2.imread(path)
+        image = cv2.resize(image_inicial, (400, 300))
+
+        """
+            La funcion que diseñamos va aqui!
+        """
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        edged = cv2.Canny(gray, 50, 100)
+        # OpenCV represents images in BGR order; however PIL represents
+        # images in RGB order, so we need to swap the channels
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # convert the images to PIL format...
+        image = Image.fromarray(image)
+        edged = Image.fromarray(edged)
+        # ...and then to ImageTk format
+        image = ImageTk.PhotoImage(image)
+        edged = ImageTk.PhotoImage(edged)
+        # if the panels are None, initialize them
+
+        if panelA is None or panelB is None:
+            # the first panel will store our original image
+            panelA = Label(image=image)
+            panelA.image = image
+            panelA.pack(side="left", padx=10, pady=10)
+            # while the second panel will store the edge map
+            panelB = Label(image=edged)
+            panelB.image = edged
+            panelB.pack(side="right", padx=10, pady=10)
+        # otherwise, update the image panels
+        else:
+            # update the pannels
+            panelA.configure(image=image)
+            panelB.configure(image=edged)
+            panelA.image = image
+            panelB.image = edged
 
 # initialize the window toolkit along with the two image panels
 root = Tk()
+root.geometry("800x500")
 panelA = None
 panelB = None
 # create a button, then when pressed, will trigger a file chooser
@@ -103,5 +108,6 @@ panelB = None
 # button the GUI
 btn = Button(root, text="Select an image", command=select_image)
 btn.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
 # kick off the GUI
 root.mainloop()
